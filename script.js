463ad36a95d4adf3dd808bc578d4e3d6af2c2cfa
Gin <181115100+bogfrog37@users.noreply.github.com>
2024-12-08 18:40:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,54 +12,53 @@ function toggleSidebar() {
   }
 }
 
+$(document).ready(function() {
+  const images = ['sheep.jpeg', 'Cows.jpeg']; // Add your image paths here
+  let currentImageIndex = 0;
 
-    $(document).ready(function() {
-      const images = ['sheep.jpeg', 'Cows.jpeg']; // Add your image paths here
-      let currentImageIndex = 0;
-    
-      function switchImage() {
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        $('#image').attr('src', images[currentImageIndex]); // Using jQuery to update the src attribute
-      }
-    
-      // Set the interval to switch images every 3 seconds (3000 milliseconds)
-      setInterval(switchImage, 3000);
-    });
+  function switchImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    $('#image').attr('src', images[currentImageIndex]); // Using jQuery to update the src attribute
+  }
 
-   // Change Link Color on Hover
-   $('.link').hover(function () {
-     $(this).css('color', '#005f73');
-   }, function () {
-     $(this).css('color', '');
-   });
+  // Set the interval to switch images every 3 seconds (3000 milliseconds)
+  setInterval(switchImage, 3000);
 
-   $(document).ready(function () {
-    // When hovering over job list items
-    $('.job-list li').hover(function () {
-      // Get the description from the 'data-description' attribute
+  // Change link color on hover
+  $('.link').hover(
+    function () {
+      $(this).css('color', '#005f73');
+    },
+    function () {
+      $(this).css('color', '');
+    }
+  );
+
+  // Inline description display on hover
+  $('.job-list li').hover(
+    function () {
       const description = $(this).data('description');
-  
-      // Display the description in the '.job-description' div
-      $('.job-description').text(description).fadeIn();
-    }, function () {
-      // Hide the description when hover ends
-      $('.job-description').fadeOut();
-    });
-  });
-  
-  $(document).ready(function() {
-    // Add a blue vertical line to the left side of the body content
-    $('body').prepend('<div id="left-line"></div>');  // Add left line
+      if (description) {
+        $(this).find('.description-container').remove(); // Remove existing description
+        $(this).append(`<div class='description-container'>${description}</div>`); // Add description
+      }
+    },
+    function () {
+      $(this).find('.description-container').remove();
+    }
+  );
 
-    // Style the left line using jQuery
-    $('#left-line').css({
-      'position': 'fixed',   // Fixed position to keep it on the left side while scrolling
-      'top': '0',            // Start from the top of the page
-      'bottom': '0',         // Extend to the bottom of the page
-      'width': '5px',        // Thickness of the line
-      'background-color': '#005f73', // Line color
-      'z-index': '9999',     // Ensure the line is on top of content
-      'left': '0'            // Position the line on the left side
-    });
+  // Add a blue vertical line to the left side of the body content
+  $('body').prepend('<div id="left-line"></div>');  // Add left line
+
+  // Style the left line using jQuery
+  $('#left-line').css({
+    'position': 'fixed',   // Fixed position to keep it on the left side while scrolling
+    'top': '0',            // Start from the top of the page
+    'bottom': '0',         // Extend to the bottom of the page
+    'width': '5px',        // Thickness of the line
+    'background-color': '#005f73', // Line color
+    'z-index': '9999',     // Ensure the line is on top of content
+    'left': '0'            // Position the line on the left side
   });
-  
+});
