c6cd9f72592ec63147a6815086fba19a76ee5e7f
Gin <181115100+bogfrog37@users.noreply.github.com>
2024-12-08 19:10:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+@@ -0,0 +1,65 @@
 function toggleSidebar() {
   const $sideNav = $('#sideNav'); // Select the side navigation element
   const $mainContent = $('#mainContent'); // Select the main content element
@@ -12,53 +13,54 @@ function toggleSidebar() {
   }
 }
 
-$(document).ready(function() {
-  const images = ['sheep.jpeg', 'Cows.jpeg']; // Add your image paths here
-  let currentImageIndex = 0;
 
-  function switchImage() {
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    $('#image').attr('src', images[currentImageIndex]); // Using jQuery to update the src attribute
-  }
-
-  // Set the interval to switch images every 3 seconds (3000 milliseconds)
-  setInterval(switchImage, 3000);
-
-  // Change link color on hover
-  $('.link').hover(
-    function () {
-      $(this).css('color', '#005f73');
-    },
-    function () {
-      $(this).css('color', '');
-    }
-  );
-
-  // Inline description display on hover
-  $('.job-list li').hover(
-    function () {
-      const description = $(this).data('description');
-      if (description) {
-        $(this).find('.description-container').remove(); // Remove existing description
-        $(this).append(`<div class='description-container'>${description}</div>`); // Add description
+    $(document).ready(function() {
+      const images = ['sheep.jpeg', 'Cows.jpeg']; // Add your image paths here
+      let currentImageIndex = 0;
+    
+      function switchImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        $('#image').attr('src', images[currentImageIndex]); // Using jQuery to update the src attribute
       }
-    },
-    function () {
-      $(this).find('.description-container').remove();
-    }
-  );
+    
+      // Set the interval to switch images every 3 seconds (3000 milliseconds)
+      setInterval(switchImage, 3000);
+    });
 
-  // Add a blue vertical line to the left side of the body content
-  $('body').prepend('<div id="left-line"></div>');  // Add left line
+   // Change Link Color on Hover
+   $('.link').hover(function () {
+     $(this).css('color', '#005f73');
+   }, function () {
+     $(this).css('color', '');
+   });
 
-  // Style the left line using jQuery
-  $('#left-line').css({
-    'position': 'fixed',   // Fixed position to keep it on the left side while scrolling
-    'top': '0',            // Start from the top of the page
-    'bottom': '0',         // Extend to the bottom of the page
-    'width': '5px',        // Thickness of the line
-    'background-color': '#005f73', // Line color
-    'z-index': '9999',     // Ensure the line is on top of content
-    'left': '0'            // Position the line on the left side
+   $(document).ready(function () {
+    // When hovering over job list items
+    $('.job-list li').hover(function () {
+      // Get the description from the 'data-description' attribute
+      const description = $(this).data('description');
+  
+      // Display the description in the '.job-description' div
+      $('.job-description').text(description).fadeIn();
+    }, function () {
+      // Hide the description when hover ends
+      $('.job-description').fadeOut();
+    });
   });
-});
+  
+  $(document).ready(function() {
+    // Add a blue vertical line to the left side of the body content
+    $('body').prepend('<div id="left-line"></div>');  // Add left line
+
+    // Style the left line using jQuery
+    $('#left-line').css({
+      'position': 'fixed',   // Fixed position to keep it on the left side while scrolling
+      'top': '0',            // Start from the top of the page
+      'bottom': '0',         // Extend to the bottom of the page
+      'width': '5px',        // Thickness of the line
+      'background-color': '#005f73', // Line color
+      'z-index': '9999',     // Ensure the line is on top of content
+      'left': '0'            // Position the line on the left side
+    });
+  });
+  
